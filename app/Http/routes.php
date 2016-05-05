5a769892d961d86873f3d1db870ac2e882f4691b
Valentin Polo <me@valentinpolo.fr>
2016-05-05 11:37:12 +0200

    Fix Content-Security-Policy

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,11 +20,10 @@ $app->get('/api-{apiUuid}', function($apiKey)
 
 $app->post('/', function(Request $request)
 {
-    $recaptcha = new \ReCaptcha\ReCaptcha(env('APP_CAPTCHA'));
-    
-    $check = $recaptcha->verify($request->get('g-recaptcha-response'));
-    if (!$check->isSuccess())
-        abort(401, "Could not check spam status.");
+//    $recaptcha = new \ReCaptcha\ReCaptcha(env('APP_CAPTCHA'));
+//    $check = $recaptcha->verify($request->get('g-recaptcha-response'));
+//    if (!$check->isSuccess())
+//        abort(401, "Could not check spam status.");
 
     $apiKey = ApiKey::create([
         'email' => $request->get('email'),
@@ -47,6 +46,7 @@ $app->get('/client', function (Request $request) use ($app)
     return $response;
 });
 
+// Version 1 here with AJAX (not working on all sites...)
 $app->post('/send', function(Request $request) use ($app)
 {
     if (!$request->has('apiKey'))
@@ -74,4 +74,38 @@ $app->post('/send', function(Request $request) use ($app)
     }
 
     return response()->json("Link sent");
+});
+
+$app->get('/send2', function(Request $request) use ($app)
+{
+    if (!$request->has('api'))
+        abort(401, 'Missing API key');
+
+    if (!ApiKey::isValid($request->get('api')))
+        abort(401, 'Invalid API key');
+
+    if (!$request->has('url'))
+        abort(401, 'Missing URL');
+
+    $apiKey = ApiKey::where('uuid', $request->get('api'))->first();
+
+    try {
+        // Send email to to address associated to API Key
+        Mail::send('emails.send', ['url' => $request->get('url'), 'title' => $request->get('title')], function ($m) use ($apiKey, $request) {
+            $m->to($apiKey->email, $apiKey->email)->subject($request->get('title'));
+        });
+
+        // Save log for legal purposes
+        Log::create(['ip' => $request->ip(), 'url' => $request->get('url'), 'title' => $request->get('title'), 'api_key' => $apiKey->id]);
+
+    } catch (Exception $e) {
+        abort(400, $e->getMessage());
+    }
+
+    header( "Content-type: image/gif");
+    header( "Expires: Wed, 5 Feb 1986 06:06:06 GMT");
+    header( "Cache-Control: no-cache");
+    header( "Cache-Control: must-revalidate");
+
+    printf ('%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%', 71,73,70,56,57,97,1,0,1,0,128,255,0,192,192,192,0,0,0,33,249,4,1,0,0,0,0,44,0,0,0,0,1,0,1,0,0,2,2,68,1,0,59);
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,11 @@
                 <input type="email" name="email" id="email" placeholder="Email address*" required>
             </div>
         </div>
-        <div class="row uniform">
-            <div class="6u$ 12u$(xsmall)">
-                <div class="g-recaptcha" data-sitekey="6Lfj9R4TAAAAAArBUavOunoPGJfXKooS0Wgky38F"></div>
-            </div>
-        </div>
+        {{--<div class="row uniform">--}}
+            {{--<div class="6u$ 12u$(xsmall)">--}}
+                {{--<div class="g-recaptcha" data-sitekey="6Lfj9R4TAAAAAArBUavOunoPGJfXKooS0Wgky38F"></div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
         <div class="row uniform">
             <div class="6u$ 12u$(xsmall)">
                 <button type="submit" class="button icon fa-envelope-o">START SENDING!</button>
