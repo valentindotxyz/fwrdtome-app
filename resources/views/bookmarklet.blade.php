c5c278c584c194ad8dac737d7efb34aa0dd3bf5e
Valentin Polo <me@valentinpolo.fr>
2016-05-05 11:48:51 +0200

    Fix Medium title

--- resources/views/bookmarklet.blade.php
+++ resources/views/bookmarklet.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     <p style="text-align: center; font-size: 26px; text-transform: uppercase;">
         <i class="fa fa-arrow-down fa-fw" aria-hidden="true"></i> <strong>Drag this button to your browser favorite's bar! <i class="fa fa-arrow-down fa-fw" aria-hidden="true"></i></strong><br /><br />
-        <a href='javascript: !function(){var e=document.createElement("img");e.setAttribute("src", "{{ env('APP_URL') }}/send2?api={{ $apiKey }}&url="+window.location.href+"&title="+encodeURI(document.title));e.style.display="none";document.body.appendChild(e);alert("Fwrdto.me: Link sent!")}();' class="button special animated flash" title="Fwrdto.me">Fwrdto.me</a>
+        <a href='javascript: !function(){var e=document.createElement("img");e.setAttribute("src", "{{ env('APP_URL') }}/send2?api={{ $apiKey }}&url="+window.location.href+"&title="+encodeURI(window.document.title));e.style.display="none";document.body.appendChild(e);alert("Fwrdto.me: Link sent!")}();' class="button special animated flash" title="Fwrdto.me">Fwrdto.me</a>
     </p>
 @endsection
