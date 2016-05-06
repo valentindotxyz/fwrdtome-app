b9e3b36bb5c9d73d8d6c1ef3f48ad8eaaf9c8857
Valentin Polo <me@valentinpolo.fr>
2016-05-06 21:14:26 +0200

    Wording

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
     <br />
 
     <form method="POST">
-        <h3 class="text-center">Get your bookmarklet, it's free!</h3>
+        <h3 class="text-center">Get your bookmarklet!</h3>
         <div class="row uniform">
             <div class="12u$ 12u$(xsmall)">
                 <input type="email" name="email" id="email" placeholder="Email address*" required>
