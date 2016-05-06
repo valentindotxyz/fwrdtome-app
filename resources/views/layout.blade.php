178a230f0388d411897a72e5894610e77bb1d62b
Valentin Polo <me@valentinpolo.fr>
2016-05-06 23:02:11 +0200

    Caching

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!--[if lte IE 8]><script src="/assets/js/ie/html5shiv.js"></script><![endif]-->
     <link rel="stylesheet" href="/assets/css/animate.css" />
-    <link rel="stylesheet" href="/assets/css/main.css?v=2" />
-    <link rel="stylesheet" href="/assets/css/custom.css" />
+    <link rel="stylesheet" href="/assets/css/main.css?v=3" />
+    <link rel="stylesheet" href="/assets/css/custom.css?v=3" />
     <!--[if lte IE 9]><link rel="stylesheet" href="/assets/css/ie9.css" /><![endif]-->
     <!--[if lte IE 8]><link rel="stylesheet" href="/assets/css/ie8.css" /><![endif]-->
     <script src='https://www.google.com/recaptcha/api.js'></script>
