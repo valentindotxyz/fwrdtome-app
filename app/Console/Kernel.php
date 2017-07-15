4b975c133538d9186bf360c0d979d596b721112c
Valentin <external.z10vpolo@decathlon.com>
2017-07-15 09:36:44 +0200

    Add cron for queued links

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,8 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->command("links:send")->dailyAt("06:00")->timezone("UTC");
     }
 
     /**
