c3cbfc444cc58eacced983315b50374749e7660c
Valentin <external.z10vpolo@decathlon.com>
2017-07-18 06:52:37 +0200

    Add legacy to preview

--- resources/views/emails/template.blade.php
+++ resources/views/emails/template.blade.php
@@ -78,7 +78,7 @@
                     <table width="100%" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
                         <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
                             <td class="aligncenter content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 12px; vertical-align: top; color: #999; text-align: center; margin: 0; padding: 0 0 20px;" align="center" valign="top">
-                                {{ env('APP_NAME') }} by <a href="https://valentin.xyz" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 12px; color: #999; text-decoration: underline; margin: 0;">Valentin Polo</a> from 🇫🇷.
+                                {{ env('APP_NAME') }} by <a href="https://valentin.xyz" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 12px; color: #999; text-decoration: underline; margin: 0;">Valentin Polo</a> from 🇫🇷
                             </td>
                         </tr>
                     </table>
