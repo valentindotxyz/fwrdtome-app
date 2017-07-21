aba70d60e72a8cf98d72e01f2661eae57c6455bc
Valentin <external.z10vpolo@decathlon.com>
2017-07-21 17:55:48 +0200

    Fix order on dates

--- app/Http/Controllers/JarvisController.php
+++ app/Http/Controllers/JarvisController.php
@@ -10,25 +10,25 @@ class JarvisController extends Controller
 {
     public function getDashboard(Request $request)
     {
-        $subscriptionsBySources = DB::table('api_keys')
+        $subscriptionsBySources = array_reverse(DB::table('api_keys')
             ->select(DB::raw('COUNT(*) AS count, DATE(created_at) AS date, source'))
             ->groupBy('date', 'source')
-            ->orderBy('date', 'ASC')
+            ->orderBy('date', 'DESC')
             ->take(50)
-            ->get();
+            ->get());
 
-        $latestSubscriptions = DB::table('api_keys')
-            ->select('email', 'status', 'source', 'created_at')
-            ->whereDate('created_at', '>=', Carbon::now()->startOfDay()->subDays(10))
-            ->orderBy('created_at', 'ASC')
-            ->get();
-
-        $sendings = DB::table('logs')
+        $sendings = array_reverse(DB::table('logs')
             ->select(DB::raw('COUNT(api_key_id) AS `count`, DATE(created_at) AS date'))
             ->groupBy('date')
-            ->orderBy('date', 'ASC')
-            ->get();
+            ->orderBy('date', 'DESC')
+            ->get());
 
+//        $latestSubscriptions = DB::table('api_keys')
+//            ->select('email', 'status', 'source', 'created_at')
+//            ->whereDate('created_at', '>=', Carbon::now()->startOfDay()->subDays(10))
+//            ->orderBy('created_at', 'ASC')
+//            ->get();
+        
         return view('jarvis.dashboard', compact('subscriptions', 'subscriptionsBySources', 'latestSubscriptions', 'sendings'));
     }
 }
