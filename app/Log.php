9af79eb010751c1b02d7203ee977c3dcbca999a2
Valentin <external.z10vpolo@decathlon.com>
2017-08-21 21:10:52 +0200

    Add quota per account

--- app/Log.php
+++ app/Log.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Enums\Configurations;
+use App\Utils\Utils;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,8 +12,13 @@ class Log extends Model
     protected $table = 'logs';
     protected $guarded = ['id'];
 
-    public static function getSendingCountForToday()
+    /**
+     * Tells if GLOBAL sending quota has been exceeded…
+     * @return bool
+     */
+    public static function sendingQuotaExceeded()
     {
-        return self::where('created_at', '>=', Carbon::today()->toDateString())->count();
+        return self::where('created_at', '>=', Carbon::today()->toDateString())
+            ->count() >= intval(Utils::getConfigurationValueForKey(Configurations::MAX_SEND));
     }
 }
